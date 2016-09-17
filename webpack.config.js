1b55023672892d3a659f0c807d514f4f0c96b3a7
Toan-Anh <nguyentoananh.10a4@gmail.com>
2016-09-17 11:58:07 +0700

    Update webpack.config.json

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     entry: './public/scripts/index.js',
     output: {
         path: path.resolve(__dirname, "public/bin"),
-        publicPath: "/bin/",
+        publicPath: "",
         filename: 'index.bundle.js'
     },
     module: {
